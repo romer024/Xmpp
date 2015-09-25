e0aa8c0d7c4723fc15a11ab08722eb575b66673d
John Garcia <johngarcia@John-Garcia.toro.dev>
2015-09-25 16:13:25 +0800

    Changed to Abstract Connection - TODO: Fix SASLError

--- XmppConnector/src/main/groovy/io/toro/xmpp/connector/XmppConnector.groovy
+++ XmppConnector/src/main/groovy/io/toro/xmpp/connector/XmppConnector.groovy
@@ -1,18 +1,42 @@
 package io.toro.xmpp.connector
 
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.XMPPConnection;
+//import org.jivesoftware.smack.Chat
+//import org.jivesoftware.smack.ChatManager
+import org.jivesoftware.smack.AbstractXMPPConnection
+import org.jivesoftware.smack.ConnectionConfiguration
+import org.jivesoftware.smack.SmackException
+import org.jivesoftware.smack.XMPPException
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode
 import org.jivesoftware.smack.tcp.XMPPTCPConnection
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration
 
 class XmppConnector {
-	public static void main(String [] args) {
-	//ConnectionConfiguration conf = new ConnectionConfiguration("localhost", 5222)
-		XMPPTCPConnectionConfiguration conf = XMPPConnectionConfiguration.builder()
-		XMPPTCPConnection connection = new XMPPTCPConnection(conf);
-		connection.connect();
-		connection.login("choi", "spiderchoi");
-		
+	static AbstractXMPPConnection  connection
+	
+    public void login(String userName, String password) throws XMPPException
+    {
+    XMPPTCPConnectionConfiguration.Builder config = XMPPTCPConnectionConfiguration.builder();
+
+    //this is server name in Open fire
+    config.setServiceName('localhost');
+    //this is host name in Open fire
+    config.setHost('localhost');
+    config.setPort(5222);
+    config.setDebuggerEnabled(true)
+	config.setSecurityMode(SecurityMode.disabled);
+    connection = new XMPPTCPConnection(config.build());
+ 
+    try {
+	 connection.connect();
+     connection.login(userName, password);
+	} catch (SmackException | IOException e) {
+  	// TODO Auto-generated catch block
+  	e.printStackTrace();
+  		}
+    }
+	
+	public static void main(String [] args){
+		def o = new XmppConnector();
+		o.login('admin','raIzel04');
 	}
-	//conn.connect();
 }
